9538c1b8297632b447452aadbb83787b16d665b8
Jordan Atanasov <jiatanasov@gmail.com>
2022-11-06 21:28:30 +0200

    debug line 1125

--- src/saltext/vmware/states/esxi.py
+++ src/saltext/vmware/states/esxi.py
@@ -1123,7 +1123,6 @@ def firewall_config(
                                 ] = f"{j} will be set to {firewall_conf[k][j]}"
                     else:
                         ret["debugger"] = old_configs[host.name][firewall_config["name"]][k]
-                        return ret
                         if old_configs[host.name][firewall_config["name"]][k] == firewall_conf[k]:
                             ret["changes"][host.name][firewall_config["name"]][
                                 k
